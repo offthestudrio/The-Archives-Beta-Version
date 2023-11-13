9a3ef8ded805813ea26f5aa04a6d2f852d621710
offthestudrio <146812318+offthestudrio@users.noreply.github.com>
2023-11-13 23:55:48 +0800

    Add files via upload

---
+++ JS/ELS_Q1_M1.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Why is Earth called 'the living planet?'",
+    answer: "D. All of the above",
+    options: [
+      "A. It sustains life.",
+      "B. It has atmosphere.",
+      "C. It has water at the surface.",
+      "D. All of the above",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which among the set of planets are called terrestrial?",
+    answer: "B. Mercury, Venus, Mars, Earth",
+    options: [
+      "A. Jupiter, Neptune, Uranus",
+      "B. Mercury, Venus, Mars, Earth",
+      "C. Earth, Venus, Mars, Jupiter",
+      "D. Jupiter, Saturn, Uranus, Neptune",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What makes the planet Earth habitable compared to other planets?",
+    answer: "D. It has liquid water",
+    options: [
+      "A. It has soil.",
+      "B. It has trees.",
+      "C. It has people.",
+      "D. It has liquid water",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What makes planet Earth habitable?",
+    answer: "D. All of the above",
+    options: [
+      "A. It has comfortable distance from the sun.",
+      "B. It has atmosphere that protects from radiation.",
+      "C. The atmosphere holds carbon dioxide and other gases.",
+      "D. All of the above",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Why is Earth called a terrestrial planet?",
+    answer: "B. It is closest to the sun.",
+    options: [
+      "A. It has life.",
+      "B. It is closest to the sun.",
+      "C. It is farther from the sun.",
+      "D. It is placed at almost the middle planets.",
+    ],
+  },
+  {
+    numb: 6,
+    question: "Which of the following best describes the surface of the planet Earth?",
+    answer: "D. mostly water surface with some areas of land",
+    options: [
+      "A. a thick layer of hydrogen gas",
+      "B. cloudy and hot with lots of volcano",
+      "C. cold, rocky, and covered with red dust",
+      "D. mostly water surface with some areas of land",
+    ],
+  },
+  {
+    numb: 7,
+    question: "Earth is said to be a unique planet. Which answer verifies the statement?",
+    answer: "D. All of the above",
+    options: [
+      "A. It supports life.",
+      "B. It is mostly covered in water.",
+      "C. Its atmosphere is mostly nitrogen and oxygen.",
+      "D. All of the above",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Which is supported and sustained by planet?",
+    answer: "A. life",
+    options: [
+      "A. life",
+      "B. a moon",
+      "C. a rocky core",
+      "D. an atmosphere",
+    ],
+  },
+  {
+    numb: 9,
+    question: "How many percent of water comprises the Earth?",
+    answer: "D. 70%",
+    options: [
+      "A. 15%",
+      "B. 30%",
+      "C. 50%",
+      "D. 70%",
+    ],
+  },
+  {
+    numb: 10,
+    question: "Which factors are needed for organisms to live on Earth?",
+    answer: "D. both A and B",
+    options: [
+      "A. temperature and nutrient",
+      "B. atmosphere and energy",
+      "C. only A",
+      "D. both A and B",
+    ],
+  },
+  {
+    numb: 11,
+    question: "What characteristics does the Earth have to maintain an organism’s body to survive?",
+    answer: "D. All of the above",
+    options: [
+      "A. Earth has a water cycle and atmosphere.",
+      "B. Earth has volcanic activities to circulate nutrients.",
+      "C. Earth has sub-surface water or molten rock that can circulate and replenish nutrients for organisms.",
+      "D. All of the above",
+    ],
+  },
+  {
+    numb: 12,
+    question: "What makes Earth similar to Venus?",
+    answer: "D. Both A and C",
+    options: [
+      "A. Earth and Venus are the right sizes to hold a sufficient atmosphere.",
+      "B. Venus’ atmosphere is 100 times thicker than Earth.",
+      "C. Among all the solid planets and moons, only Earth, Venus, and Titan have significant atmospheres.",
+      "D. Both A and C",
+    ],
+  },
+  {
+    numb: 13,
+    question: "Which statement about the Earth’s energy in sustaining life is incorrect?",
+    answer: "A. Earth survives in too little energy.",
+    options: [
+      "A. Earth survives in too little energy.",
+      "B. Earth is in the right distance from the sun",
+      "C. Earth is kept warm by an insulating atmosphere.",
+      "D. Earth has magnetic field to protect from solar radiation.",
+    ],
+  },
+  {
+    numb: 14,
+    question: "Which distinct characteristic made Earth different from any other planet?",
+    answer: "C. It is the only planet that has a large amount of liquid water.",
+    options: [
+      "A. It is the only rocky planet.",
+      "B. It is the only planet that turns around in space.",
+      "C. It is the only planet that has a large amount of liquid water.",
+      "D. It is the only planet that changes its structure.",
+    ],
+  },
+  {
+    numb: 15,
+    question: "What does Earth offer to human in order to sustain life?",
+    answer: "D. Both A and B",
+    options: [
+      "A. Earth has abundant plant life.",
+      "B. Earth has sufficient nutrients from ocean to land.",
+      "C. Only A",
+      "D. Both A and B",
+    ],
+  },
+];
